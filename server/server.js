09cf150f958919502b978765c5b078ca8a96fc0f
Talal Malick <m.talalmalick2014@gmail.com>
2019-05-11 00:05:08 +0500

    Add disconnect and connect event listener socketIO

--- server/server.js
+++ server/server.js
@@ -1,14 +1,28 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const SocketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app);
+
+var io = SocketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, (err) => {
-    console.log(`Server is up on port ${port}`);
-})
+// io.on lets you register for new event, we can listen for new event and do something when its happen
+// connection event lets you listen for new connection, do something when new connection comes in.
+io.on('connection', (socket) => {
+    console.log('New User Connected');
 
-console.log(publicPath);
+    // every time browser closes this message shows up
+    socket.on('disconnect', () => {
+        console.log('User was disconnected');
+    });
+});
+
+server.listen(port, (err) => {
+    console.log(`Server is up on port ${port}`);
+});
